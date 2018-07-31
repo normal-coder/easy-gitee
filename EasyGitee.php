29419b4b01faa6a83b8c42833110b48fd3f43e79
诺墨 <normal@normalcoder.com>
2018-07-31 16:33:30 +0800

    m

---
+++ EasyGitee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace normalcoder;
+
+class EasyGitee
+{
+    protected $a;
+
+    protected $b;
+
+    public function __construct(int $a, int $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function addTogether()
+    {
+        return $this->a + $this->b;
+    }
+}
